caec00df9c2cbbaa6ffd54bfe2bacfd3e665b439
oognuyh <oognuyh@gmail.com>
2021-05-05 20:43:03 +0900

    Initial commit

---
+++ src/main/java/com/example/minishop_spring/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.minishop_spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.example.minishop_spring.controller", "com.example.minishop_spring.service"})
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/", ".jsp");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
