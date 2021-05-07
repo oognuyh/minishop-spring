204518513a6a427790743869d4085f56818f29f9
oognuyh <oognuyh@gmail.com>
2021-05-07 23:47:25 +0900

    fix: change ComponentScan

--- src/main/java/com/example/minishop_spring/config/RootConfig.java
+++ src/main/java/com/example/minishop_spring/config/RootConfig.java
@@ -1,9 +1,20 @@
 package com.example.minishop_spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan("com.example.minishop_spring")
+@ComponentScan(
+        basePackages = "com.example.minishop_spring",
+        excludeFilters = {
+                @Filter(
+                        type = FilterType.ANNOTATION,
+                        classes = { Controller.class }
+                )
+        }
+)
 public class RootConfig {
 }
--- src/main/java/com/example/minishop_spring/config/WebConfig.java
+++ src/main/java/com/example/minishop_spring/config/WebConfig.java
@@ -1,7 +1,10 @@
 package com.example.minishop_spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -9,12 +12,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan({"com.example.minishop_spring.controller", "com.example.minishop_spring.service"})
+@ComponentScan(
+        basePackages = "com.example.minishop_spring",
+        includeFilters = {
+                @Filter(
+                        type = FilterType.ANNOTATION,
+                        classes = { Controller.class }
+                )
+        }
+)
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.jsp("/WEB-INF/", ".jsp");
+        registry.jsp("/WEB-INF/views/", ".jsp");
     }
 
     @Override
